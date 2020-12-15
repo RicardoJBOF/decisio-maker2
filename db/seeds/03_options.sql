fc0463f2f05cdd6b0dad5b6aad323b5508b9eac6
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-15 19:39:38 +0000

    Create seeds

---
+++ db/seeds/03_options.sql
@@ -0,0 +1,15 @@
+INSERT INTO options
+  (name, poll_id)
+VALUES
+  ('hike', 1),
+  ('picnic', 1),
+  ('beach', 1),
+  ('restaurant', 1),
+  ('Casablanca', 4),
+  ('Gone With the Wind', 4),
+  ('Wizard of Oz', 4),
+  ('The Ring', 4),
+  ('Hamilton to London', 3),
+  ('Ottawa to Mississauga', 3),
+  ('Montreal to Halifax', 3),
+  ('Toronto to Sao Paulo', 3);
