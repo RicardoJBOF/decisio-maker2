4b199ef2435aba7d84b9d271d67887fb3e9edb08
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-16 23:18:40 +0000

    Add button to render more options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,36 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$(document).ready(function () {
+
+  let last_text = 3;
+  let present_letter = 'C'
+
+  //CREATE NEW FILD (INDEX.HTML)
+  function nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1).toUpperCase();
+  }
+
+  const createField = () => {
+
+    let field = $(`
+    <div class="form-group form-group-answer"><p>${present_letter})</p>
+      <textarea class="form-control" id="txtAreaanswer${last_text}"></textarea>
+    </div>
+    `);
+
+    present_letter = nextChar(present_letter);
+    last_text++;
+    return field;
+  };
+
+  const renderNewField = () => {
+    $('#new-field').append(createField());
+  };
+
+  $("#addMoreAnswers").click(function () {
+    renderNewField();
+  });
+
+
+
+
+
 });
