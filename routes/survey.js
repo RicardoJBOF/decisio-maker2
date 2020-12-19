cd1f9940c8350b820b4a23636c36db11b6e559a0
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-19 21:16:11 +0000

    Add function to access id information on database

--- routes/survey.js
+++ routes/survey.js
@@ -3,7 +3,38 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
+  const accessQuestion = function(user) {
+    return new Promise(resolve => {
+      let pollId = parseInt(user.pollId);
+      let result = {};
+      result.poll = pollId;
+      db.query(`
+    SELECT id, name
+    FROM polls
+    WHERE id = ${pollId};
+    `, (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+        result.pollname = res.rows;
+        db.query(`
+      SELECT id, name
+      FROM options
+      WHERE poll_id = ${pollId};
+      `, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          result.options = res.rows;
+          resolve(result);
+        });
+      });
+    });
+  };
+
   router.get("/:id", (req, res) => {
+    const user = req.params;
+
     res.render("survey");
   });
 
