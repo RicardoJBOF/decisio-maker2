2f73deae4c855677e07d74a24e5a7128c57ac1e6
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-22 16:27:44 -0500

    Add getUserByEmail function

--- routes/register.js
+++ routes/register.js
@@ -6,5 +6,41 @@ module.exports = (db) => {
     res.render("register");
   });
 
+  const getUserByEmail = function(email) {
+    const query = {
+      text: `SELECT *
+      FROM creators
+      WHERE email = ($1);`,
+      values: [email]
+    }
+    return db.query(query)
+    .then(res => res.rows[0])
+    .then( err => err)
+  };
+
+  router.post('/', (req, res) => {
+    const user = req.body;
+
+    console.log(user)
+
+    // getUserWithEmail(user.email)
+    //   .then(userRes => {
+    //     if (userRes !== undefined) {
+    //       return res.status(400).send("User already registered, try login");
+    //     } else if (!user.email || !user.password) {
+    //       return res.status(400).send("Email or Password missing");
+    //     } else {
+    //       user.password = bcrypt.hashSync(user.password, 12);
+    //       registerUser(user)
+    //         .then(user => {
+    //           req.session.userId = user;
+    //           res.redirect('/');
+    //         })
+    //         .catch(e => res.send(e));
+    //     }
+    //   });
+  });
+
+
   return router;
 };
