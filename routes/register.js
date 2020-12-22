7f9f9cafb0445c3506afade0b9d4bbc964626780
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-22 17:55:48 -0500

    Add registerUser function and send data whether user is register or not

--- routes/register.js
+++ routes/register.js
@@ -18,10 +18,22 @@ module.exports = (db) => {
     .then( err => err)
   };
 
+  const registerUser = (user) => {
+    const query = {
+      text: `INSERT INTO creators (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *`,
+      values: [user.name, user.email, user.password]
+    }
+    return db.query(query)
+      .then(res => res.rows[0])
+      .catch(err => console.log(err));
+  };
+
   router.post('/', (req, res) => {
     const data = req.body;
     getUserByEmail(data.email).then( user => {
-      res.send(user)
+      !user ? registerUser(data).then(user => res.send(user)) : res.send({answer: false});
     })
   });
 
