9ad1b76cc8cf6f8892af7fc6593b3994cb1abe84
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-22 19:05:14 -0500

    Add bcrypt to registerd password

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -11,11 +12,12 @@ module.exports = (db) => {
       text: `SELECT *
       FROM creators
       WHERE email = ($1);`,
-      values: [email]
-    }
-    return db.query(query)
-    .then(res => res.rows[0])
-    .then( err => err)
+      values: [email],
+    };
+    return db
+      .query(query)
+      .then((res) => res.rows[0])
+      .then((err) => err);
   };
 
   const registerUser = (user) => {
@@ -23,18 +25,23 @@ module.exports = (db) => {
       text: `INSERT INTO creators (name, email, password)
       VALUES ($1, $2, $3)
       RETURNING *`,
-      values: [user.name, user.email, user.password]
-    }
-    return db.query(query)
-      .then(res => res.rows[0])
-      .catch(err => console.log(err));
+      values: [user.name, user.email, bcrypt.hashSync(user.password, 12)],
+    };
+    return db
+      .query(query)
+      .then((res) => res.rows[0])
+      .catch((err) => console.log(err));
   };
 
-  router.post('/', (req, res) => {
+  router.post("/", (req, res) => {
     const data = req.body;
-    getUserByEmail(data.email).then( user => {
-      !user ? registerUser(data).then(user => res.send(user)) : res.send();
-    })
+    getUserByEmail(data.email).then((user) => {
+      !user
+        ? registerUser(data).then((user) => {
+            res.redirect("../");
+          })
+        : res.send();
+    });
   });
 
   return router;
