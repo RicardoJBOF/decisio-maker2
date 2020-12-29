3b7ef7dd39691878c4ec0225b306bd257476eb5c
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-12-29 11:01:57 -0500

    Admin page done, front and backend

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,43 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
+  const accessQuestionPerUser = (user) => {
+    const id = parseInt(user.id);
+    const data = { id };
+
+    const query = {
+      text: `SELECT polls.id AS pollsid, polls.name AS question, creators.name AS creators, creators.id AS creatorsid
+      FROM polls
+      INNER JOIN creators ON creators.id = polls.creator_id
+      WHERE creators.id = ${id};`,
+    };
+    return db
+      .query(query)
+      .then((res) => {
+        data.creators = res.rows[0].creators;
+        data.creatorsid = res.rows[0].creatorsid;
+        data.pollsid = [];
+        data.question = [];
+        for (let i = 0; i < res.rows.length; i++) {
+          data.pollsid.push(res.rows[i].pollsid);
+          data.question.push(res.rows[i].question);
+        }
+        console.log(data);
+        return data;
+      })
+      .catch((err) => err);
+  };
+
   router.get("/:id", (req, res) => {
-    res.render("admin");
+    const id = req.params;
+    accessQuestionPerUser(id).then((data) => {
+      const templateVars = {
+        data,
+      };
+      console.log(templateVars);
+      res.render("admin", templateVars);
+    });
   });
 
   return router;
